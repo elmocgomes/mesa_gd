495d5001c4ccb4e515d9c3c7620424a0be22f78b
Elmo Gomes <elmo.gomes@outlook.com>
2020-05-27 15:30:08 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mesa_gd',
+      version='0.0.1',
+      description='Agent-Based package to build social and economic models based on Generalized Darwinism ',
+      url='http://github.com/elmocgomes/mesa_gd',
+      author='Elmo Gomes',
+      author_email='elmo.gomes@outlook.com',
+      license='MIT',
+      packages=['mesa_gd'],
+      zip_safe=False)
